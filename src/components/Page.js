f78a1e80ed0f930fde52c08430bff69a4d1411b3
duongtdn <duongtdn@gmail.com>
2017-01-09 13:58:14 +0700

    Init Page component

--- src/components/Page.js
+++ src/components/Page.js
@@ -4,8 +4,22 @@ import React from 'react'
 
 import BaseComponent from './BaseComponent'
 
+/**
+ * Page component
+ * @extends BaseComponent 
+ * */
 export default class extends BaseComponent {
-  
+  /**
+   * Render a Page component
+   * @param {function} renderHeader - render Header of Page
+   * @param {function} renderFooter - renderFooter of Page
+   * @param {function} renderFixed - render a fixed component on Page
+   * @param {function} renderModel - render a model cover entire of Page
+   * @param {string} modifier - specify modifier style for Page
+   * @param {function} onInit - function will be invoked after Page is mounted
+   * @param {function} onShow - function will be invoked after page has been shown
+   * @param {function} onHide - function will be invoked after page has been hide
+   */
   constructor(props) {
     super(props);
   }
@@ -15,8 +29,24 @@ export default class extends BaseComponent {
   }
 
   render() {
+    const header = this.props.renderHeader ? this.props.renderHeader() : null;
+    const footer = this.props.renderFooter ? this.props.renderFooter() : null;
+    const fixed  = this.props.fixed ? this.props.fixed() : null;
+    const model  = this.props.model ? this.props.model() : null;
     return(
-      <sg-page >
+      <sg-page > 
+        <div className = 'page'>
+          <div className = 'page_background' />
+          <div className = 'page_header'> 
+            {header} 
+          </div>
+          <div className = 'page_content'>
+            {this.props.children}
+          </div>
+          <div className = 'page_footer'> 
+            {footer} 
+          </div>
+        </div>
       </sg-page>
     );
   }
---
+++ src/exporter.js
@@ -0,0 +1,6 @@
+"use strict"
+
+import React from 'react'
+
+export { default as BaseComponent } from './components/BaseComponent'
+export { default as Page } from './components/Page'
