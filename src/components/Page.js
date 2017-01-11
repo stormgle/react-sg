c5adb090dd48b08582babb9aad789be0400495a6
duongtdn <duongtdn@gmail.com>
2017-01-11 16:57:32 +0700

    minor fix plus add style

--- src/components/Page.js
+++ src/components/Page.js
@@ -14,7 +14,10 @@ export default class extends BaseComponent {
    * @param {function} renderHeader - render Header of Page
    * @param {function} renderFooter - renderFooter of Page
    * @param {function} renderFixed - render a fixed component on Page
-   * @param {function} renderModel - render a model cover entire of Page
+   * @param {function} renderModal - render a model cover entire of Page
+   * @param {boolean} hideHeader - hide page header behind background
+   * @param {boolean} hideFooter - hide page footer behind background
+   * @param {boolean} hideModal - hide modal behind background
    * @param {string} modifier - specify modifier style for Page
    * @param {function} onInit - function will be invoked after Page is mounted
    * @param {function} onShow - function will be invoked after page has been shown
@@ -29,25 +32,25 @@ export default class extends BaseComponent {
   }
 
   render() {
-    const themeContainer = 'w3-container';
+    const style = 'w3';
     const header = this.props.renderHeader ? this.props.renderHeader() : null;
     const footer = this.props.renderFooter ? this.props.renderFooter() : null;
     const fixed  = this.props.renderFixed ? this.props.renderFixed() : null;
-    const model  = this.props.renderModel ? this.props.renderModel() : null;
+    const model  = this.props.renderModal ? this.props.renderModal() : null;
     return(
       <sg-page > 
         <div className = 'page'>
           <div className = 'page_background' />
-          <div className = {`page_model ${themeContainer}`} > 
+          <div className = {`page_model ${style}-container ${this.props.hideModal ? 'hide' : ''}`} > 
             {model} 
           </div>
-          <div className = {`page_header ${themeContainer}`} > 
+          <div className = {`page_header ${style}-container`} > 
             {header} 
           </div>
-          <div className = {`page_content ${themeContainer}`} >
+          <div className = {`page_content ${style}-container`} >
             {this.props.children}
           </div>
-          <div className = {`page_footer ${themeContainer}`}> 
+          <div className = {`page_footer ${style}-container`}> 
             {footer} 
           </div>
         </div>
